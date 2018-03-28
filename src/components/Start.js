e449d67b45ade76a85b0f11f804218e5e4ab089e
rocciescobar <roccisen.88@gmail.com>
2018-03-28 14:37:05 -0500

    Añade información al componente start

--- src/components/Start.js
+++ src/components/Start.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Start = () => (
   <div>
-    <h3 className="mg">Inicio</h3>
+    <h3 className="mt-4 font-weight-bold">Introducción</h3>
+    <p className="text-justify">Esta guía de estilos fue diseñada con el uso de componentes React que pueden combinarse para ayudar a los desarrolladores a crear los diferentes productos digitales de Laboratoria.</p>
+    <h3 className="mt-4 font-weight-bold">Instalación</h3>
+    <p>Ejecute el siguiente comando usando npm:</p>
+    <div class="card">
+      <div className="card-body">
+        <pre className="m-0">
+          <code className="language-css" data-lang="css">
+            <span className="k">npm install laboratoria-style-guide</span>
+          </code>
+        </pre>
+      </div>
+    </div>
+    <div className="mt-4 mb-5">
+      <a href="https://www.npmjs.com/package/laboratoria-style-guide" className="btn back-green1 font-weight-bold" role="button" aria-pressed="true">Más información</a>
+    </div>
+    <h3 className="mt-4 font-weight-bold">Uso</h3>
+    <p>Cada componente incluye información para ayudarlo a implementarlos.</p>
   </div>
 )
 
