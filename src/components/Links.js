ef1280782633b090f2a8d61c254ebf2c7637b137
melissayauri <yaurimelissa@gmail.com>
2018-03-28 06:49:31 -0500

    mejorando sección links

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,24 +4,36 @@ const Links = () => (
   <div>
     <h3 className="mg">Links</h3>
     <hr/>
-    <div className="flex text-justify">
-      <div >
-        <h3 className="">Leer más</h3>
-        <p>Leer más enlaces aparecen en la línea con párrafos de texto truncado.Al hacer clic en ellos puede revelar texto oculto en la línea o cargar una nueva página</p>
+      <div class="flex">
+        <div class="mr-5">
+          <h3 className="">Leer más</h3>
+          <p className="">Leer más enlaces aparecen en la línea con párrafos de texto truncado.Al hacer clic en ellos puede revelar texto oculto en la línea o cargar una nueva página</p>
+        </div>
+        <div class="">
+          <p className="">Ejemplo</p>
+          <p className="">Si quieres leer más sobre el examen, ingresa aquí. Ingresa aquí</p>
+        </div>
       </div>
-      <div>
-        <h3 className="">Ejemplo</h3>
-        <p>si quiere leer más sobre el examen, ingresa aquí.<span className="color-fucsia">Ingresa aqui</span></p>
+      <div class="flex">
+        <div class="mr-5">
+          <h3 className="">Más links</h3>
+          <p className="">Se usan más enlaces para vincular contenido adicional en otra página.Por lo general, aparecen debajo de una lista de cosas como "negocios relacionados"</p>
+        </div>
+        <div class="">
+          <p className="">Ejemplo</p>
+          <p className="">Sigue nuestro blog</p>
+        </div>
       </div>
-      <div >
-        <h3 className="">Más links</h3>
-        <p>Se usan más enlaces para vincular contenido adicional en otra página</p>
+      <div class="flex">
+        <div class="mr-5">
+          <h3 className="">Pseudo link</h3>
+          <p className="">Forzar cualquier elemento para que parezca un enlace</p>
+        </div>
+        <div class="">
+          <p className="">Ejemplo</p>
+          <p className="">La start-up peruana que conquisto a Obama y a Zuckerberg</p>
+        </div>
       </div>
-      <div>
-        <h3 className="">Ejemplo</h3>
-        <p>Nuestro blog</p>
-      </div>
-    </div>
   </div>
 )
 
